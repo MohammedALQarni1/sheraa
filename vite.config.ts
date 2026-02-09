73fa696cd7dee9668b079bf7a077e152908f6f50
MohammedALQarni1 <eng_m.alqarni@hotmail.com>
2026-02-09 18:18:09 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // -----------------------------------------------------------------------
-  // هام: قم بتغيير 'REPO_NAME' في السطر التالي إلى اسم المستودع الخاص بك على GitHub
-  // مثال: إذا كان رابط المشروع https://github.com/username/my-project
-  // اجعل القيمة: '/my-project/'
-  // -----------------------------------------------------------------------
-  // إذا كان الموقع هو صفحة المستخدم الرئيسية (username.github.io)، اجعل القيمة '/'
-  base: '/REPO_NAME/', 
+  // تم تحديث المسار ليطابق اسم المستودع الخاص بك كما يظهر في الرابط
+  base: '/sheraa/', 
   build: {
     outDir: 'dist',
   }
